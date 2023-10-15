8df4fbfe0bbccbc967af4b73de50ac24615303f3
attila.mate <attila.mate@hotmail.com>
2023-10-15 13:00:45 +0200

    Js
    teszt

---
+++ script.js
@@ -0,0 +1,30 @@
+/*let keresztNev = 'Pisti';
+console.log(keresztNev);
+let vezetekNev = 'Kis';
+console.log(vezetekNev);
+
+let kor = '32';
+
+let nagyKoru = 'true'; 
+*/
+
+/*let keresztNev = 'Pisti';
+let kor = 32;
+
+console.log(keresztNev + ' ' + kor);
+
+let vezetekNev, felnott;
+vezetekNev = 'kovács';
+//felnott = 'true';
+
+console.log(vezetekNev  + 'felnőtt? ' + felnott); */
+
+/*let osszeg= 10 + 33;
+console.log(osszeg)*/
+
+
+let i = 2;
+for (let i= 0; i <10; i++) {
+    console.log(i);
+}
+
